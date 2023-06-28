494580bac43d8717204dba508fe2ff43530fe535
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-06-28 10:21:21 +0530

    DailyWage.cs

---
+++ DailyWage.cs
@@ -0,0 +1,36 @@
+
+using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int isPresent = 1;
+            int empWage = 0;
+            const int EMP_RATE_PER_HR = 20;
+
+            //welcome message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int employeeAttendance=random.Next(0, 2); //0 or 1
+            if (employeeAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                //empHrs = 0;
+            }
+            //UC2
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Daily Wage is: " + empWage);
+            Console.ReadLine();
+        }
+    }
+}
